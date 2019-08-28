04cfaabff7098808cf95b0ea3b8a8ab564596575
wanghai <bigtofar@gmail.com>
2019-08-28 10:13:53 +0800

    第二次修改

--- src/main/java/com/puyuan/service/UserService.java
+++ src/main/java/com/puyuan/service/UserService.java
@@ -1,4 +1,7 @@
 package com.puyuan.service;
 
+import com.puyuan.pojo.User;
+
 public interface UserService {
+    public User findOne(int uid);
 }
--- src/main/java/com/puyuan/service/impl/UserServiceImpl.java
+++ src/main/java/com/puyuan/service/impl/UserServiceImpl.java
@@ -1,7 +1,12 @@
 package com.puyuan.service.impl;
 
+import com.puyuan.pojo.User;
 import com.puyuan.service.UserService;
 
 public class UserServiceImpl implements UserService{
 
+    @Override
+    public User findOne(int uid) {
+        return null;
+    }
 }
